36d6a88bd338fca23c3fac4abec9d2c20a419936
mkrebs <michael.krebs@edvschule-plattling.de>
2022-03-07 08:37:35 +0100

    Server configuration

--- src/main/java/Fertigungsstrasse/Server.java
+++ src/main/java/Fertigungsstrasse/Server.java
@@ -15,54 +15,52 @@ public class Server {
     public static void main(String[] args) {
         Server.openConnection();
     }
+
     public static void openConnection() {
-        try{
+        try {
             ServerSocket sv = new ServerSocket(42000);
             Socket connection = sv.accept();
 
             boolean stop = false;
             String message = "";
 
+            String msg = "A01.1;A;2";
+
+
             PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
             BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             output.println("Server started!");
-
-            while (!stop){
-                output.println(br.readLine());
+            int counter = 0;
+            while (!stop) {
+                //output.println(br.readLine());
                 message = input.readLine();
-                if (message.contains("abgeschlossen")){
-
-                }
 
                 //eingabe wird verarbeitet
-                if (message.contains("exit"))
-                {
+                if (message.contains("exit")) {
                     stop = true;
-                    output.println("Bye");
-                }
-                else if (message.contains("ping"))
-                {
-                    output.println("Pong!");
-                }
-                else
-                {
-                    output.println("You have to write ping!");
+                    output.println("Stop Server");
+                } else if (message.contains("bereit")) {
+                    output.println(msg);
+                } else if (message.contains("fehler")) {
+                    if (counter < 3) {
+                        counter++;
+                        output.println(msg);
+                    } else {
+                        counter = 0;
+                    }
+                } else {
+                    output.println("Bitte Status senden!");
                 }
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
     }
 
-    public String getString(){
-        Query q =
-        return "";
-    };
-
-    public void send(){
+    public void send() {
 
     }
 }
