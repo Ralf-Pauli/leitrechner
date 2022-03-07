3c3b8a085b60ae5fa9f9efecebff2b5dc3c426d8
mkrebs <michael.krebs@edvschule-plattling.de>
2022-03-07 11:16:08 +0100

    Update Server class

--- src/main/java/Fertigungsstrasse/Server.java
+++ src/main/java/Fertigungsstrasse/Server.java
@@ -1,6 +1,5 @@
 package Fertigungsstrasse;
 
-import javax.management.Query;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,46 +17,66 @@ public class Server {
 
     public static void openConnection() {
         try {
-            ServerSocket sv = new ServerSocket(42000);
+            ServerSocket sv = new ServerSocket(43000);
             Socket connection = sv.accept();
 
             boolean stop = false;
-            String message = "";
+            String answer = "";
 
-            String msg = "A01.1;A;2";
-
+            String auftragMsg = "";
 
             PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
             BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Connected");
             output.println("Server started!");
             int counter = 0;
             while (!stop) {
-                //output.println(br.readLine());
-                message = input.readLine();
+                answer = input.readLine();
 
                 //eingabe wird verarbeitet
-                if (message.contains("exit")) {
+                if (answer == null) {
+                    output.println("Bitte Status senden!");
+                } else if (answer.contains("exit")) {
                     stop = true;
                     output.println("Stop Server");
-                } else if (message.contains("bereit")) {
-                    output.println(msg);
-                } else if (message.contains("fehler")) {
-                    if (counter < 3) {
-                        counter++;
-                        output.println(msg);
-                    } else {
-                        counter = 0;
-                    }
+                } else if (answer.contains("bereit")) {
+                    writeStatus();
+                    auftragMsg = getMessage();
+                    System.out.println("Message: " + auftragMsg);
+                    output.println(auftragMsg);
+                } else if (answer.contains("fehler")) {
+                    writeStatus();
                 } else {
-                    output.println("Bitte Status senden!");
+                    output.println("Nachricht fehlerhaft");
                 }
             }
+            console.close();
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
+    }
+
+    public static Socket clOpen(Socket conn, ServerSocket sv){
+        try {
+            conn = sv.accept();
+            return conn;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+
+    public static String getMessage(){
+        return "A01.1;A;2";
+    }
+
+    public static void writeStatus(){
+        System.out.println("Status: ");
     }
 
     public void send() {
