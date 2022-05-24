0b44e59786ce4707b8d165c0eefdae3593b56f4a
rpauli <Ralf.Pauli@edvschule-plattling.de>
2022-05-24 08:23:48 +0200

    Add Callback to getData

--- LRWebapplication/public/javascript/connect.js
+++ LRWebapplication/public/javascript/connect.js
@@ -6,46 +6,65 @@ let connection = mysql.createConnection({
     database: 'Carlos'
 });
 
+
+var data = undefined;
+getData(null, 2, null, "offen", printJson);
+
+async function convertToJson(data){
+    return Object.values(JSON.parse(JSON.stringify(data)));
+}
+
+function printJson() {
+    console.log(convertToJson(data));
+}
+
 let count = 0;
-getData(null,2,null,"fertig");
 
-function getData(id = null, produkt_id = null, produkt_anzahl = null, status = null) {
-   let results = "";
-    connection.connect(function (error) {
-        if (error) throw error;
-        let query = "SELECT id, produkt_id, produkt_anzahl, status FROM Auftrag";
+function getData(id = null, produkt_id = null, produkt_anzahl = null, status = null, callback) {
+    //return new Promise(data => {
+        connection.connect(function (error) {
+            if (error) throw error;
+            let query = "SELECT id, produkt_id, produkt_anzahl, status FROM Auftrag";
 
-        if (id !== null) {
-            if (count === 0) {
-                query += " Where"
+            if (id !== null) {
+                if (count === 0) {
+                    query += " Where";
+                }
+                query += " id = " + mysql.escape(id);
+                count++;
             }
-            query += " id = " + mysql.escape(id);
-            count++;
-        }
 
-        if (produkt_id !== null) {
-            query = check(query);
-            query += " produkt_id = " + mysql.escape(produkt_id);
-            count++;
-        }
+            if (produkt_id !== null) {
+                query = check(query);
+                query += " produkt_id = " + mysql.escape(produkt_id);
+                count++;
+            }
 
-        if (produkt_anzahl !== null) {
-            query = check(query);
-            query += " produkt_anzahl = " + mysql.escape(produkt_anzahl);
-            count++;
-        }
+            if (produkt_anzahl !== null) {
+                query = check(query);
+                query += " produkt_anzahl = " + mysql.escape(produkt_anzahl);
+                count++;
+            }
 
-        if (status !== null) {
-            query = check(query);
-            query += " status = " + mysql.escape(status);
-        }
+            if (status !== null) {
+                query = check(query);
+                query += " status = " + mysql.escape(status);
+            }
 
-        connection.query(query, function (err, result, fields) {
-            if (err) throw err;
-            connection.end();
-            console.log(result);
+            connection.query(query, function (err, result, fields) {
+                if (err) throw err;
+                try {
+                    data = result;
+                    //data(result);
+                    callback();
+                } catch (error) {
+                    //data({});
+                    throw error;
+                }
+                connection.end();
+            });
         });
-    });
+    //});
 }
 
 function check(query) {
@@ -57,4 +76,4 @@ function check(query) {
     return query;
 }
 
-module.exports = {getData};
+module.exports = { getData };
