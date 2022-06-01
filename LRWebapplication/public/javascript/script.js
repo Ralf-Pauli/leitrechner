f43a86d1257b0be22bf5de30630eca399dcb58c2
mkrebs <michael.krebs@edvschule-plattling.de>
2022-06-01 09:51:06 +0200

    Update Look and Feel

--- LRWebapplication/public/javascript/script.js
+++ LRWebapplication/public/javascript/script.js
@@ -1,61 +1,35 @@
-const json = "[{id: 30,produkt_id: 2,produkt_anzahl: 2,status: 'fertig'}, {id: 45,produkt_id: 2,produkt_anzahl: 3,status: 'fertig'}]";
-//const obj = JSON.parse(json);
-//console.log(obj.id)
-
 function generate_table(json) {
-    // get the reference for the body
-    let auftrag = JSON.parse(JSON.stringify(json));
+    let jsonObject = JSON.parse(JSON.stringify(json));
 
+    // gets body element
     let body = document.getElementsByTagName("body")[0];
     // creates a <table> element and a <tbody> element
     if (document.getElementById("datatable") != null) {
         removeElement("datatable");
     }
 
+    (document.getElementById("loader")).style.visibility = "visible";
+
     let tbl = document.createElement("table");
     tbl.setAttribute("id", "datatable")
+    tbl.setAttribute("class", "styled-table")
     // let tblBody = document.createElement("tbody");
-    let row = '<tr><td>Auftrags-ID</td><td></td></tr>'
+    let row = '<tr><th>Auftrags ID</th><th>Produkt ID</th><th>Anzahl</th><th>Status</th></tr>'
+    tbl.innerHTML += row;
 
     // creating all cells
-    for (let i = 0; i < auftrag.length; i++) {
+    for (let i = 0; i < jsonObject.length; i++) {
+
         let row = `<tr>
-                        <td>${auftrag[i].id}</td>
-                        <td>${auftrag[i].produkt_id}</td>
-                        <td>${auftrag[i].produkt_anzahl}</td>
-                        <td>${auftrag[i].status}</td>
-                    </tr>`
+            <td>${jsonObject[i].id}</td>
+            <td>${jsonObject[i].produkt_id}</td>
+            <td>${jsonObject[i].produkt_anzahl}</td>
+            <td>${jsonObject[i].status}</td>
+            </tr>`
         tbl.innerHTML += row
-        /*
-        // creates a table row
-        let row = document.createElement("tr");
-
-        for (let j = 0; j < 2; j++) {
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-
-            if (i == 0) {
-                var cell = document.createElement("th");
-            } else {
-                var cell = document.createElement("td");
-            }
-            let cellText = document.createTextNode("cell in row " + i + ", column " + j);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
-*/
-
-        // add the row to the end of the table body
-        // tblBody.appendChild(row);
     }
-
-    // put the <tbody> in the <table>
-    // tbl.appendChild(tblBody);
-    // appends <table> into <body>
     body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
-    // tbl.setAttribute("border", "2");
+    (document.getElementById("loader")).style.visibility = "hidden";
 }
 
 function removeElement(id) {
